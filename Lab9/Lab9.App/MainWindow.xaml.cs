3c42a1c7d878e4fcd7bff0780c16b59b982740cb
zuirseime <vovakibets88@gmail.com>
2024-05-17 11:58:13 +0300

    Add UI to Lab9.App

--- Lab9/Lab9.App/MainWindow.xaml.cs
+++ Lab9/Lab9.App/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Lab9.App;
 /// <summary>
@@ -16,5 +7,9 @@ namespace Lab9.App;
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+    }
+
+    private void FindAssignmentMatrix(object sender, RoutedEventArgs e) {
+
     }
 }
