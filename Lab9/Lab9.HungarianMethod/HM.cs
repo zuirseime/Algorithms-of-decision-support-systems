33eb5f121173d92c3f108af6ed58337adb5b683a
zuirseime <vovakibets88@gmail.com>
2024-05-17 14:35:56 +0300

    refactor hungarian algorithm

--- Lab9/Lab9.HungarianMethod/HM.cs
+++ Lab9/Lab9.HungarianMethod/HM.cs
@@ -35,6 +35,41 @@ public class HM {
         return (_matrix, GetCost(defaultMatrix));
     }
 
+
+
+    private void Decrease(bool byRow, int outerCount, int innerCount) {
+        string type = byRow ? "row" : "column";
+
+        Log.WriteLine($"Search for the minimum elements in each {type} and subtract it from each element in the {type}:");
+        double[] mins = FindMinimums(byRow, outerCount);
+
+        Subtract(mins, byRow, outerCount, innerCount);
+
+        Log.WriteLine("\nThe cost matrix after subtracting:");
+        Log.WriteLine(_matrix);
+    }
+
+    private double[] FindMinimums(bool byRow, int outer) {
+        double[] result = new double[outer];
+
+        for (int o = 0; o < outer; o++) {
+            double min = (byRow ? _matrix.Data.MinInRow(o) : _matrix.Data.MinInColumn(o)).Value;
+            result[o] = min;
+            string type = byRow ? "row" : "column";
+            Log.WriteLine($"The minimum value in {type} {o + 1} is {min}");
+        }
+        return result;
+    }
+
+    private void Subtract(double[] mins, bool byRow, int outer, int inner) {
+        for (int o = 0; o < outer; o++) {
+            for (int i = 0; i < inner; i++) {
+                if (byRow) _matrix[o, i] -= mins[o];
+                else _matrix[i, o] -= mins[o];
+            }
+        }
+    }
+
     private double GetCost(Matrix matrix) {
         double cost = 0;
         List<double> additives = [];
@@ -112,39 +147,6 @@ public class HM {
         Log.WriteLine(_matrix);
     }
 
-    private void Decrease(bool byRow, int outerCount, int innerCount) {
-        string type = byRow ? "row" : "column";
-
-        Log.WriteLine($"Search for the minimum elements in each {type} and subtract it from each element in the {type}:");
-        double[] mins = FindMinimums(byRow, outerCount);
-
-        Subtract(mins, byRow, outerCount, innerCount);
-
-        Log.WriteLine("\nThe cost matrix after subtracting:");
-        Log.WriteLine(_matrix);
-    }
-
-    private double[] FindMinimums(bool byRow, int outer) {
-        double[] result = new double[outer];
-
-        for (int o = 0; o < outer; o++) {
-            double min = (byRow ? _matrix.Data.MinInRow(o) : _matrix.Data.MinInColumn(o)).Value;
-            result[o] = min;
-            string type = byRow ? "row" : "column";
-            Log.WriteLine($"The minimum value in {type} {o + 1} is {min}");
-        }
-        return result;
-    }
-
-    private void Subtract(double[] mins, bool byRow, int outer, int inner) {
-        for (int o = 0; o < outer; o++) {
-            for (int i = 0; i < inner; i++) {
-                if (byRow) _matrix[o, i] -= mins[o];
-                else _matrix[i, o] -= mins[o];
-            }
-        }
-    }
-
     private bool CullOffLines() {
         Log.WriteLine("Search for the matrix of optimal assignments:");
         Log.WriteLine("Cross out all zeros:");
@@ -156,8 +158,8 @@ public class HM {
         while (true) {
             count++;
 
-            if (col) CullOffVertical(rows, cols);
-            else CullOffHorizontal(rows, cols);
+            if (col) CullOff(rows, cols, false);
+            else CullOff(rows, cols, true);
 
             if (!_matrix.Data.Any(i => i.Value == 0 && i.State == State.None))
                 break;
@@ -172,61 +174,50 @@ public class HM {
         return count == _matrix.Width;
     }
 
-    private void CullOffHorizontal(int rows, int cols) {
-        double max = double.MinValue;
-        int rMax = 0;
-        for (int row = 0; row < rows; row++) {
-            var value = _matrix.Data.CountInRow(i => i.Value == 0 && i.State == State.None, row);
-            if (max < value) {
-                max = value;
-                rMax = row;
-            }
-        }
+    private void CullOff(int rows, int cols, bool horizontal) {
+        int iMax = GetMaxZeroNumber(rows, cols, horizontal);
 
         bool hasZero = false;
-        for (int col = 0; col < cols; col++) {
-            if (_matrix[rMax, col].Value == 0 && _matrix[rMax, col].State == State.None && !hasZero) {
-                _matrix.Data[rMax, col].State = State.Zero;
+        for (int i = 0; i < (horizontal ? cols : rows); i++) {
+            MatrixItem item = horizontal ? _matrix[iMax, i] : _matrix[i, iMax];
+            if (item.Value == 0 && item.State == State.None && !hasZero) {
+                if (horizontal) _matrix.Data[iMax, i].State = State.Zero;
+                else _matrix.Data[i, iMax].State = State.Zero;
+
                 hasZero = true;
                 continue;
             }
 
-            if (_matrix[rMax, col].State != State.Zero) {
-                _matrix.Data[rMax, col].State =
-                    _matrix[rMax, col].State != State.Vertical
-                    ? State.Horizontal : State.Overlaped;
-            }
+            if (horizontal) CullOffZeros(iMax, i, State.Vertical, State.Horizontal);
+            else CullOffZeros(i, iMax, State.Horizontal, State.Vertical);
         }
 
         if (_full) Log.WriteLine(_matrix.ToString(true));
     }
 
-    private void CullOffVertical(int rows, int cols) {
+    private int GetMaxZeroNumber(int rows, int cols, bool horizontal) {
+        Func<int, double> countFunc = horizontal ?
+            row => _matrix.Data.CountInRow(i => i.Value == 0 && i.State == State.None, row) :
+            col => _matrix.Data.CountInColumn(i => i.Value == 0 && i.State == State.None, col);
+
         double max = double.MinValue;
-        int cMax = 0;
-        for (int col = 0; col < cols; col++) {
-            var value = _matrix.Data.CountInColumn(i => i.Value == 0 && i.State == State.None, col);
+        int iMax = 0;
+
+        for (int i = 0; i < (horizontal ? rows : cols); i++) {
+            double value = countFunc(i);
             if (max < value) {
                 max = value;
-                cMax = col;
+                iMax = i;
             }
         }
 
-        bool hasZero = false;
-        for (int row = 0; row < rows; row++) {
-            if (_matrix[row, cMax].Value == 0 && _matrix[row, cMax].State == State.None && !hasZero) {
-                _matrix.Data[row, cMax].State = State.Zero;
-                hasZero = true;
-                continue;
-            }
+        return iMax;
+    }
 
-            if (_matrix[row, cMax].State != State.Zero) {
-                _matrix.Data[row, cMax].State =
-                    _matrix[row, cMax].State != State.Horizontal
-                    ? State.Vertical : State.Overlaped;
-            }
+    private void CullOffZeros(int row, int col, State oldState, State newState) {
+        if (_matrix[row, col].State != State.Zero) {
+            _matrix.Data[row, col].State = _matrix[row, col].State 
+                                        != oldState ? newState : State.Overlaped;
         }
-
-        if (_full) Log.WriteLine(_matrix.ToString(true));
     }
 }
