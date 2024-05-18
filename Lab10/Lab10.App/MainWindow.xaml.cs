0a4c558a6f9d269e1d9b56cd2fccb4d5e56cf34c
zuirseime <vovakibets88@gmail.com>
2024-05-18 11:23:15 +0300

    change MainWindow

--- Lab10/Lab10.App/MainWindow.xaml.cs
+++ Lab10/Lab10.App/MainWindow.xaml.cs
@@ -1,20 +1,29 @@
-﻿using System.Text;
+﻿using Lab10.GridPlanning;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lab10.App;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window {
+    private static string PAGES = "Pages";
+
+    private Activity[] _activities = null!;
+
     public MainWindow() {
         InitializeComponent();
+        Navigate("Home.xaml");
+    }
+
+    private void ChangePage(object sender, RoutedEventArgs e) {
+        if (sender is Button button) {
+            Navigate(button.Tag.ToString()!);
+        }
+    }
+
+    private void Navigate(string path) {
+        _frame.Navigate(new(Path.Combine(PAGES, path), UriKind.Relative));
     }
 }
