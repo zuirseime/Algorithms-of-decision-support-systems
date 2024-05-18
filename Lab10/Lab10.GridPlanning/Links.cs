07783a33fc3c46a382d40e58d17206f161ff8d19
zuirseime <vovakibets88@gmail.com>
2024-05-18 15:25:26 +0300

    delete lab10

--- Lab10/Lab10.GridPlanning/Links.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab10.GridPlanning;
-public struct Links(string data) {
-    public int[] Data { get; set; } = data.Split([",", ", "], StringSplitOptions.TrimEntries)
-                                          .Select(int.Parse).ToArray();
-
-    public override string ToString() {
-        return string.Join(", ", Data);
-    }
-}
