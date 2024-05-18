bf891829e1987096341bdd4b70504f74a60a7452
zuirseime <vovakibets88@gmail.com>
2024-05-18 17:31:57 +0300

    finish Lab10 bussines logic

--- Lab10/Lab10.ProjectSchedule/ProjectTask.cs
+++ Lab10/Lab10.ProjectSchedule/ProjectTask.cs
@@ -1,18 +1,29 @@
 namespace Lab10.ProjectSchedule;
 
-public class ProjectTask(int start, int duration) {
+public class ProjectTask(List<ProjectTask> previous, int duration, int workers) {
     private static int counter;
 
     public int Id { get; set; } = ++counter;
-    public List<ProjectTask> Previous { get; set; } = [];
+    public List<ProjectTask> Previous { get; set; } = previous;
     public List<ProjectTask> Next { get; set; } = [];
-    public Segment Early { get; set; } = new Segment(start, start + duration);
+    public Segment Early { get; set; }
     public Segment Late { get; set; }
     public int Duration { get; set; } = duration;
     public int Reserve { get; set; }
     public bool Critical { get; set; } = false;
+    public int Workers { get; set; } = workers;
 
-    public void SetLate(int finish) => Late = new Segment(finish - Duration, finish);
+    public void SetEarly() {
+        int max = Previous.Count > 0 ? Previous.Max(i => i.Early.Finish) : 0;
+        Early = new Segment(max, max + Duration);
+    }
+    public void SetLate() {
+        int min = Next.Count > 0 ? Next.Min(i => i.Late.Start) : Early.Finish;
+        Late = new Segment(min - Duration, min);
+
+        CalculateReserve();
+    }
+
     public void CalculateReserve() {
         Reserve = Late.Finish - Early.Finish;
         Critical = Reserve == 0;
--- Lab10/Lab10.ProjectSchedule/Scheduler.cs
+++ Lab10/Lab10.ProjectSchedule/Scheduler.cs
@@ -1,3 +1,49 @@
 ﻿namespace Lab10.ProjectSchedule;
 public class Scheduler {
+    private List<ProjectTask> _tasks = [];
+
+    public (string, int) Run(List<string> tasks) {
+        FillTaskList(tasks);
+
+        _tasks.ForEach(task => task.Previous.ForEach(prev => prev.Next.Add(task)));
+
+        _tasks.Reverse<ProjectTask>().ToList().ForEach(t => t.SetLate());
+        string way = string.Join('-', _tasks.Where(t => t.Critical).Select(i => i.Id));
+        int days = _tasks[^1].Early.Finish;
+
+        return (way, days);
+    }
+
+    private void FillTaskList(List<string> tasks) {
+        foreach (var (task, args) in GetEnumerable(tasks)) {
+            if (args.Length != 4)
+                throw new ArgumentException($"Task #{tasks.IndexOf(task) - 1} has incorrect number of arguments");
+
+            int[] prevs = args[1].Split(',').Select(int.Parse).ToArray();
+            _ = int.TryParse(args[2], out int duration);
+            _ = int.TryParse(args[3], out int workers);
+
+            List<ProjectTask> previous = [];
+            if (!prevs.Contains(0)) {
+                foreach (var id in prevs) {
+                    previous.Add(_tasks[id - 1]);
+                }
+            }
+
+            ProjectTask projectTask = new(previous, duration, workers);
+            projectTask.SetEarly();
+
+            _tasks.Add(projectTask);
+        }
+    }
+
+    private static IEnumerable<(string task, string[] args)> GetEnumerable(List<string> tasks) 
+        => from task in tasks let args = task.Split().Select(p => p.Trim()).ToArray() select (task, args);
+
+    private void ConnectPrevious(int[] previous, ProjectTask projectTask) {
+        foreach (var prev in from item in previous let prev = _tasks[item - 1] select prev) {
+            prev.Next.Add(projectTask);
+            projectTask.Previous.Add(prev);
+        }
+    }
 }
